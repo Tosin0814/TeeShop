c261de609023dc8706efe761919f5e6c53205d28
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2024-01-20 09:02:37 -0500

    Size home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,16 @@ import './Home.css'
 export default function Home() {
   return (
     <div className='Home'>
-      <br /><br />
-      <h3>Home</h3>
+      <div className='contentContainer d-flex'>
+        <aside className='sideBar'>
+          <p>All Products</p>
+          <p>Categories</p>
+          <p>Recommended</p>
+        </aside>
+        <div className='mainContent'>
+          <h3>Home</h3>
+        </div>
+      </div>
     </div>
   )
 }
